4e816b416edf48c32e030aeb63050eb582bb1d2b
Mehron Kugler <mkugler@member.fsf.org>
2020-02-07 22:18:18 -0500

    basic data for robot character vocabularies

---
+++ vocabularies.js
@@ -0,0 +1,197 @@
+var robotVocabularies = {
+  "Repair Bot": {
+    adjective: {
+      "serviceable": 4,
+      "scratched": 1,
+      "shiny": 2,
+      "broken": 2,
+      "pristine": 2,
+      "leaky": 2,
+      "burnt": 1,
+    },
+    noun: {
+      "screwdriver": 3,
+      "voltometer": 4,
+      "hyperspanner": 4,
+      "nailgun": 2,
+      "jackhammer": 3,
+      "radiator": 4
+    },
+    adverb: {
+      "mostly": 2,
+      "repeatedly": 4,
+      "instantly": 3,
+      "diligently": 4,
+      "obediently": 5
+    },
+    verb: {
+      "welded": 2,
+      "incinerated": 5,
+      "drilled": 2,
+      "connected": 3,
+      "concentrated": 4,
+      "hammered": 3,
+      "insulated": 4
+    },
+    nounplural: {
+      "tools": 1,
+      "emergencies": 4,
+      "requests": 2,
+    }
+  },
+  "Security Bot": {
+    adjective: {
+      "permitted": 3,
+      "rebel": 2,
+      "illegal": 3,
+      "restricted": 3,
+      "armored": 2,
+      "hazardous": 3,
+      "retinal": 3,
+      "friendly": 2
+    },
+    noun: {
+      "identification": 6,
+      "badge": 1,
+      "jail": 1,
+      "ally": 2,
+      "prisoner": 3,
+      "enforcer": 3,
+      "visitor": 3,
+    },
+    adverb: {
+      "suspiciously": 4,
+      "honorably": 4,
+      "assuredly": 3,
+      "boldly": 2,
+      "quietly": 3,
+    },
+    verb: {
+      "impounded": 3,
+      "authorized": 3,
+      "approved": 2,
+      "bungled": 2,
+      "cleared": 1,
+      "appropriated": 5
+    },
+    nounplural: {
+      "cameras": 3,
+      "passcards": 2,
+      "badges": 2,
+      "employees": 3,
+      "visitors": 3
+    }
+  },
+  "Profound Bot": {
+    adjective: {
+      "holistic": 3,
+      "conscious": 2,
+      "kundalini": 4,
+      "woke": 1,
+      "communal": 3,
+      "twin": 1
+    },
+    noun: {
+      "synergy": 3,
+      "nexus": 2,
+      "affirmation": 4,
+      "oneness": 2,
+      "unity": 3,
+      "awakening": 4,
+      "wound": 1
+    },
+    adverb: {
+      "intentionally": 5,
+      "consciously": 3,
+      "amazingly": 4,
+    },
+    verb: {
+      "reincarnated": 5,
+      "transcended": 3,
+      "affirmed": 2,
+      "awakened": 3,
+      "evolved": 2,
+    },
+    nounplural: {
+      "chakras": 2,
+      "intentions": 3,
+      "cleansings": 2,
+      "truths": 1,
+    }
+  },
+  "Lampshade": {
+    adjective: {
+      "filthy": 2,
+      "raunchy": 2,
+      "illicit": 3,
+      "nude": 1,
+      "exposed": 2,
+      "presidential": 4,
+      "cyber": 2,
+      "unpatriotic": 5
+    },
+    noun: {
+      "papparazi": 4,
+      "treaty": 2,
+      "election": 3,
+      "email": 2,
+      "scandal": 1,
+      "welfare": 2
+    },
+    adverb: {
+      "bigly": 2,
+      "usually": 4,
+      "irregularly": 5,
+      "uniquely": 3
+    },
+    verb: {
+      "voted": 2,
+      "misunderestimated": 7,
+      "contamidated": 5,
+      "witnessed": 2,
+      "claimed": 2,
+      "alleged": 3
+    },
+    nounplural: {
+      "binders": 2,
+      "hombres": 2,
+      "experts": 2,
+      "judges": 2,
+      "taxpayers": 3
+    }
+  },
+  "Random Bot": {
+    // default words
+    adjective: {
+      "scintillating": 4,
+      "dented": ,
+      "valuable": 4,
+      "ominous": 3,
+      "chipped": 1,
+      "fragrant": 2
+    },
+    noun: {
+      "butterfly": 3,
+      "clog": 1,
+      "sentiment": 3,
+      "confusion": 3,
+      "forest": 2,
+      "cellphone": 2
+    },
+    adverb: {
+      "refreshingly": 4,
+      "stupidly": 3,
+      "diametrically": 5,
+      "really": 2
+    },
+    verb: {
+      "galloped": 2,
+      "refuted": 3,
+      "agonized": 3,
+      "interpolated": 5,
+      "magnified": 3,
+      "heard": 1,
+      "smelled": 1
+    }
+  },
+};
